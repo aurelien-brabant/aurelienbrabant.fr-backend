4b6c53ee2356f3377814bc666c1d69781304539b
abrabant <abrabant@student.42.fr>
2021-12-19 02:48:12 +0100

    Add better table initialization structure

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const db = require('../src/database').pool;
+const db = require('../src/database/database').pool;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
---
+++ src/database/init_table.js
@@ -0,0 +1,70 @@
+const sqlCreateUserAccountTable = `CREATE TABLE IF NOT EXISTS user_account (
+		user_id				SERIAL PRIMARY KEY,
+		email				VARCHAR(70) UNIQUE NOT NULL,
+		username			VARCHAR(25) UNIQUE NOT NULL,
+		role				SMALLINT DEFAULT 0,
+		is_email_verified	BOOLEAN DEFAULT false,
+		is_activated		BOOLEAN DEFAULT false,
+		firstname			VARCHAR(50),
+		lastname			VARCHAR(50),
+		password			VARCHAR(100),
+		account_creation_ts	TIMESTAMP NOT NULL,
+		last_login_ts		TIMESTAMP
+	);`;
+
+const sqlCreateBlogpostTable = `CREATE TABLE IF NOT EXISTS blogpost (
+		blogpost_id			SERIAL PRIMARY KEY,
+		title				VARCHAR(100) NOT NULL,
+		author_id			INTEGER REFERENCES user_account(user_id) NOT NULL,
+		description			VARCHAR(300) NOT NULL,
+		content				TEXT NOT NULL,
+		publication_date	TIMESTAMP NOT NULL,
+		cover_image_path	VARCHAR(255)
+	);`;
+
+const sqlCreateBlogpostCommentTable = `CREATE TABLE IF NOT EXISTS blogpost_comment (
+		blogpost_comment_id	SERIAL PRIMARY KEY,
+		author				INTEGER REFERENCES user_account(user_id),
+		guest_name			VARCHAR(25),
+		up_vote				INTEGER DEFAULT 0,
+		down_vote			INTEGER DEFAULT 0,
+		is_approved			BOOLEAN DEFAULT false,
+		approval_ts			TIMESTAMP,
+		content				TEXT NOT NULL,
+		post_creation_ts	TIMESTAMP NOT NULL
+	);`;
+
+/*
+ * Email verification token 
+ */
+
+const sqlCreateEmailVerificationTokenTable = `CREATE TABLE IF NOT EXISTS email_verification_token (
+		email_verification_token_id SERIAL PRIMARY KEY,
+		issuer_user_id				INTEGER REFERENCES user_account(user_id) NOT NULL,
+		expiration_ts				TIMESTAMP NOT NULL,
+		lifetime					INTERVAL
+	);`
+
+/*
+ * Events
+ */
+
+const sqlCreateUserEventTable = `CREATE TABLE IF NOT EXISTS user_event (
+		user_id				INTEGER REFERENCES user_account(user_id) NOT NULL,
+		tstz				TIMESTAMPTZ NOT NULL,
+		payload				TEXT
+	);`;
+
+const sqlCreateUserLoginEventTable = `CREATE TABLE IF NOT EXISTS user_login_event () INHERITS(user_event);`;
+
+module.exports = [
+	sqlCreateUserAccountTable,
+	
+	sqlCreateBlogpostTable,
+	sqlCreateBlogpostCommentTable,
+
+	sqlCreateEmailVerificationTokenTable,
+	
+	sqlCreateUserEventTable,
+	sqlCreateUserLoginEventTable,
+];
