56e34213cde0dd7335ce38798e3f0ade77a2fc12
abrabant <abrabant@student.42.fr>
2021-12-19 18:45:31 +0100

    Implement basic CRUD operations for users

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-const userServices = require("../src/database/user");
+const userServices = require("../services/users");
 
 /* GET users listing. */
 router.get("/", async function (_req, res, _next) {
@@ -12,13 +12,45 @@ router.get("/", async function (_req, res, _next) {
   }
 });
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const { email, username, password: plainTextPassword } = req.body;
 
   try {
-    return res.json(await userServices.createUser(email, username, plainTextPassword));
-  } catch(e) {
+    return res.json(
+      await userServices.createUser(email, username, plainTextPassword)
+    );
+  } catch (e) {
     console.error(e);
+    return res.status(500).send("Internal Server Error");
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const userData = await userServices.findUserById(id);
+
+    console.log(userData);
+
+    if (!userData) {
+      return res.status(404).send("Not found");
+    }
+
+    return res.json(userData);
+  } catch (e) {
+    return res.status(500).send("Internal Server Error");
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+   const deleted = await userServices.removeUserById(id);
+
+    return res.status(200).send();
+  } catch {
     return res.status(500).send("Internal Server Error");
   }
 });
---
+++ services/users.js
@@ -0,0 +1,128 @@
+const bcrypt = require("bcrypt");
+const db = require("../src/database/database").pool;
+
+// Helpers {{{
+
+const findUserBy = async (searchBy, searchValue) => {
+	const res = await db.query(
+		`SELECT user_id, email, username, role, is_email_verified, is_activated, firstname, lastname, account_creation_ts, last_login_ts
+		FROM user_account
+		WHERE ${searchBy} = $1
+		LIMIT 1
+		;`,
+		[searchValue]
+	);
+
+	/* no-match */
+
+	if (!res.rows.length) {
+		return null;
+	}
+
+	const row = res.rows[0];
+
+	return {
+		userId: row.user_id,
+		email: row.email,
+		username: row.username,
+		firstname: row.firstname,
+		lastname: row.lastname,
+		role: row.role,
+		isEmailVerified: row.is_email_verified,
+		isActivated: row.is_email_activated,
+		accountCreationTs: row.account_creation_ts,
+		lastLoginTs: row.last_login_ts,
+	};
+};
+
+// }}}
+
+
+// find {{{
+
+const findUserByEmail = async (email) => {
+	return await findUserBy("email", email);
+};
+
+const findUserByUsername = async (username) => {
+	return await findUserBy("username", username);
+};
+
+const findUserById = async (id) => {
+	return await findUserBy("user_id", id);
+};
+
+const findUsers = async (limit = 100) => {
+	const res = await db.query(
+		`SELECT user_id, email, username, role, is_email_verified, is_activated
+		FROM user_account
+		LIMIT $1
+	;`,
+		[limit]
+	);
+
+	return res.rows.map((row) => ({
+		userId: row.user_id,
+		email: row.email,
+		username: row.username,
+		role: row.role,
+		isEmailVerified: row.is_email_verified,
+		isActivated: row.is_activated,
+	}));
+};
+
+// }}}
+
+const createUser = async (email, username, password) => {
+	const hash = await bcrypt.hash(password, 10);
+
+	const res = await db.query(
+		`INSERT
+		INTO user_account(email, username, password, account_creation_ts)
+		VALUES ($1, $2, $3, $4)
+		RETURNING email, username, account_creation_ts
+	`,
+		[email, username, hash, new Date()]
+	);
+
+	return {
+		email,
+		username,
+		accountCreationTs: res.rows[0].account_creation_ts,
+	};
+};
+
+/**
+ * @return {bool} true if targeted user has been successfully deactivated/deleted false otherwise.
+ */
+
+const removeUserById = async (id, onlyDeactivate = true) => {
+	if (onlyDeactivate) {
+		const res = await db.query(
+			`UPDATE user_account
+			SET is_activated = false
+			WHERE user_id = $1
+			RETURNING is_activated
+		;`,
+			[id]
+		);
+
+		return res.rows.length ? res.rows[0].activated : false;
+	}
+
+	const res = await db.query(`
+		DELETE FROM user_account
+		WHERE user_id = $1
+	;`, [id]);
+
+	return !!res.rowCount;
+};
+
+module.exports = {
+	findUsers,
+	findUserById,
+	findUserByUsername,
+	findUserByEmail,
+	removeUserById,
+	createUser,
+};
