f51cea1d0d7ecab461fb179111a886e80e05f14f
abrabant <abrabant@student.42.fr>
2021-12-21 04:33:50 +0100

    Enable CORS for any domain, using the cors npm middleware

--- app.ts
+++ app.ts
@@ -8,14 +8,25 @@ import usersRouter from './routes/users';
 import blogpostsRouter from './routes/blogposts';
 import bodyParser from 'body-parser';
 
+import cors from 'cors';
+
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
+
+/*app.use((_req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*'); // TODO: only allow frontend to perform CORS
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+})
+*/;
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- src/database/init_table.ts
+++ src/database/init_table.ts
@@ -28,7 +28,7 @@ const sqlCreateBlogpostTable: SqlQuery = `CREATE TABLE IF NOT EXISTS blogpost (
 		release_ts			TIMESTAMP NOT NULL,
 		last_edit_ts		TIMESTAMP NOT NULL,
 		cover_image_path	VARCHAR(255),
-		privacy				VARCHAR(20) DEFAULT PRIVATE
+		privacy				VARCHAR(20) DEFAULT 'PRIVATE'
 	);`;
 
 const sqlCreateBlogpostCommentTable: SqlQuery = `CREATE TABLE IF NOT EXISTS blogpost_comment (
